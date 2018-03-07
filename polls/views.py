a8a6ba4f61fa9391557f36127b71007a93ae61c3
tim <2225189600@qq.com>
2018-03-07 17:32:03 +0800

    templet, sample form finished

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,9 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, Http404
 from django.template import loader
+from django.shortcuts import render,get_object_or_404
+from .models import Question
 
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -20,7 +14,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("you're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
